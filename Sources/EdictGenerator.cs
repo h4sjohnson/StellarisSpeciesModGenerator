b792f6bbb400e84c8ec13db94b4a0ab57cd8dcfa
HasJohnson <hasjohnson@outlook.com>
2023-08-19 14:51:20 +0800

    edict localisation->localisation

--- Sources/EdictGenerator.cs
+++ Sources/EdictGenerator.cs
@@ -17,25 +17,6 @@
 
 			Directory.CreateDirectory(@"output\common\edicts");
 			File.WriteAllLines(@$"output\common\edicts\1_{ModWorkload.modNamespace}_rename_edict.txt", lines);
-
-			/**	localisation for edicts	**/
-			for (int LanguageIdx = 0; LanguageIdx < ModWorkload.languages.Length; LanguageIdx++)
-			{
-				string Language = ModWorkload.languages[LanguageIdx];
-				List<string> locLines = new()
-				{
-					$"l_{Language}:"
-				};
-
-				for (int EdictIdx = 0; EdictIdx < ModWorkload.edictWorkload.edictNames.Count; EdictIdx++)
-				{
-					locLines.Add($" {ModWorkload.edictWorkload.edictNames[EdictIdx]}:0 \"{ModWorkload.edictWorkload.edictLocTexts[LanguageIdx][EdictIdx]}\"");
-					locLines.Add($" {ModWorkload.edictWorkload.edictNames[EdictIdx]}_desc:0 \"{ModWorkload.edictWorkload.edictDescLocTexts[LanguageIdx][EdictIdx]}\"");
-				}
-
-				Directory.CreateDirectory(@$"output\localisation\{Language}");
-				File.WriteAllLines(@$"output\localisation\{Language}\{ModWorkload.modNamespace}_edicts_l_{Language}.yml", locLines, System.Text.Encoding.UTF8);
-			}
 		}
 	}
 }
---
+++ Sources/LocalisationGenerator.cs
@@ -0,0 +1,44 @@
+﻿namespace SpeciesModGenerator
+{
+    public class LocalisationGenerator
+    {
+        public static void Generate()
+        {
+            /**	localisation	**/
+            List<string[]> localisationSheet = new();
+            using (var fileStream = File.Open(@"localisation.csv", FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (var reader = new StreamReader(fileStream, System.Text.Encoding.UTF8))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    if (line != null)
+                    {
+                        line = line.Replace("_Namespace_", ModWorkload.modNamespace);
+                        localisationSheet.Add(line.Split(','));
+                    }
+                }
+            }
+            for (int LanguageIdx = 0; LanguageIdx < ModWorkload.languages.Length; LanguageIdx++)
+            {
+                string Language = ModWorkload.languages[LanguageIdx];
+                if (Language == "")
+                    continue;
+                List<string> locLines = new()
+                {
+                    $"l_{Language}:"
+                };
+
+                for (int i = 1; i < localisationSheet.Count; i++)
+                {
+                    var text = localisationSheet[i][LanguageIdx + 1];
+                    text = text.Replace("[[LocalizedNamespace]]", ModWorkload.localizedNamespaces[LanguageIdx]);
+                    locLines.Add($" {localisationSheet[i][0]}:0 \"{text}\"");
+                }
+
+                Directory.CreateDirectory(@$"output\localisation\{Language}");
+                File.WriteAllLines(@$"output\localisation\{Language}\{ModWorkload.modNamespace}_l_{Language}.yml", locLines, System.Text.Encoding.UTF8);
+            }
+        }
+    }
+}
